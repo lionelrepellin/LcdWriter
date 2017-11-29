ff320720ffc131b680fc8c3ebbd5e65ac86fe502
Lionel <lionel.repellin@gmail.com>
2017-11-29 02:56:15 +0100

    Fix: missing dash

--- LcdWriter/Digits/Two.cs
+++ LcdWriter/Digits/Two.cs
@@ -5,7 +5,8 @@
 		public override char[,] GetDigits()
 		{
 			Digits[0, 1] = '_';
-			Digits[1, 2] = '|';
+            Digits[1, 1] = '_';
+            Digits[1, 2] = '|';
 			Digits[2, 0] = '|';
 			Digits[2, 1] = '_';
 
